f9e7b8928330690df93354b3364901a0fcbde218
Michael Lopez <malopez1@email.neit.edu>
2023-05-01 14:00:54 -0400

    Fixing

--- lopezMidterm/src/App.jsx
+++ lopezMidterm/src/App.jsx
@@ -1,17 +1,21 @@
-import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Products from './Products';
+//import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '/components/Home';
+import Products from '/components/Products';
+import ProductDetails from '/components/ProductDetails';
 
 function App() {
   return (
-    <div>
-      {/* Persistent toolbar or navigation drawer component */}
+    <Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={ProductDetails} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
+
